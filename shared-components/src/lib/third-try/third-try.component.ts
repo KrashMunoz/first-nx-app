815a9fd6df0de842b5589586b36b669a04ed4b32
Krash <krash.munoz@gmail.com>
2024-11-16 15:15:34 -0500

    added unordered list for users state

--- shared-components/src/lib/third-try/third-try.component.ts
+++ shared-components/src/lib/third-try/third-try.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, TitleCasePipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { State, usersFeature } from 'src/app/user.reducer';
 
 @Component({
   selector: 'lib-third-try',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './third-try.component.html',
   styleUrl: './third-try.component.scss',
 })
-export class ThirdTryComponent {}
+export class ThirdTryComponent {
+  user$: Observable<State>;
+
+  constructor(private store: Store) {
+    this.user$ = this.store.select(usersFeature.selectUsersState);
+    this.user$.subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.error(err)
+    })
+  }
+
+}
