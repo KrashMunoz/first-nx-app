5d567a851f96211e8701e59315c370cb6d35518d
Krash <krash.munoz@gmail.com>
2024-11-16 20:57:48 -0500

    created grid layout component

---
+++ shared-components/src/lib/grid-layout/grid-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-grid-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grid-layout.component.html',
+  styleUrl: './grid-layout.component.scss',
+})
+export class GridLayoutComponent {}
