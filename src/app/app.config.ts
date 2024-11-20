a5bfe4754633147836837b97c5c9f67c13abf105
Krash <krash.munoz@gmail.com>
2024-11-19 21:43:04 -0500

    added hash location strategy to router; fixed page height css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideStore, StoreModule } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import * as fromUser from './user.reducer';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideEffects(),
-    provideStore({users: fromUser.reducer}),
+    provideStore({ users: fromUser.reducer }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
