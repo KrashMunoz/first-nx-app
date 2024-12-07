8c775c3dbc448f478bdc03a806b989acdfc7aae9
Krash <krash.munoz@gmail.com>
2024-12-07 13:13:48 -0500

    updated form in home page

--- shared-components/src/lib/test-component/test-component.component.ts
+++ shared-components/src/lib/test-component/test-component.component.ts
@@ -2,27 +2,37 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store, StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
-import { UserActions } from '../../../../src/app/user.actions' 
+import { UserActions } from '../../../../src/app/user.actions';
 import { User } from 'src/app/user.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'lib-test-component',
   standalone: true,
-  imports: [CommonModule, StoreModule, FormsModule],
+  imports: [
+    CommonModule,
+    StoreModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './test-component.component.html',
   styleUrl: './test-component.component.scss',
 })
 export class TestComponentComponent {
   userForm: string | undefined;
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
   submitUser() {
     const formModel = {
-      user: this.userForm
+      user: this.userForm,
     };
-    // console.log({formModel});
+    console.log({ formModel });
     if (!!formModel.user) {
-      const user: User = {id : formModel.user};
-      this.store.dispatch(UserActions.addUser({user}));
+      const user: User = { id: formModel.user };
+      this.store.dispatch(UserActions.addUser({ user }));
     }
   }
 }
