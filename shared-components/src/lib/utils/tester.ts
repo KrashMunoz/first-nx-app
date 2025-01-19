b983a05811a0bcfc0df93b52f10eff1ada2a7867
KrashMunoz <krash.munoz@gmail.com>
2025-01-18 23:25:00 -0500

    implemented facades into tester function

--- shared-components/src/lib/utils/tester.ts
+++ shared-components/src/lib/utils/tester.ts
@@ -1,47 +1,86 @@
+import { EventParamsBuilder, EventProdParamsBuilder, EventReceiverParamsBuilder, GqlParamsBuilder } from "./builder";
 import { EventParamsFactory, ParamsFactory } from "./factory";
 
+/**
+ * Facade for Parameter Factory and Builders
+ */
+class ParamsFacade {
+    public factory: ParamsFactory;
+    public defaultBuilder: GqlParamsBuilder;
+    public basic: GqlParamsBuilder;
+    public advanced: GqlParamsBuilder;
+
+    constructor() {
+        this.factory = new ParamsFactory();
+        this.defaultBuilder = this.factory.createBuilder('default');
+        this.basic = this.factory.createBuilder('basic');
+        this.advanced = this.factory.createBuilder('advanced');
+    }
+
+    /**
+     * Log Param Builders
+     */
+    testParams() {
+        this.basic
+            .addString('Name', 'Krash')
+            .addString('Description', 'The best description ever.')
+            .addInteger('ID', 1);
+
+        console.log({
+            basicBuilder: this.basic,
+            advancedBuilder: this.advanced,
+            defaultBuilder: this.defaultBuilder
+        });
+
+    }
+}
+
+/**
+ * Facade for Event Factory and Builders
+ */
+class EventFacade {
+    public eventFactory: EventParamsFactory;
+    public event: EventParamsBuilder;
+    public eventProducer: EventProdParamsBuilder;
+    public eventReceiver: EventReceiverParamsBuilder;
+
+    constructor() {
+        this.eventFactory = new EventParamsFactory();
+        this.event = this.eventFactory.createBuilder('event');
+        this.eventProducer = this.eventFactory.createBuilder('producer');
+        this.eventReceiver = this.eventFactory.createBuilder('receiver');
+    }
+
+    /**
+     * Log Event Builders
+     */
+    testEvents() {
+        this.event
+            .addID(1)
+            .addName('Event1');
+        this.eventProducer
+            .addID(1)
+            .addName('EventProducer1');
+        this.eventReceiver
+            .addID(1)
+            .addName('EventReceiver1');
+
+        console.log({
+            event: this.event,
+            eventProducer: this.eventProducer,
+            eventReceiver: this.eventReceiver
+        });
+    }
+}
+
+/**
+ * Function to test all design patterns
+ */
 export const testerFn = () => {
-    // Generic Params Factory
-    const factory = new ParamsFactory();
-    const basicBuilder = factory.createBuilder('basic');
-    const advancedBuilder = factory.createBuilder('advanced');
-    const defaultBuilder = factory.createBuilder('default');
-
-    // Domain Specific Factory
-    const eventFactory = new EventParamsFactory();
-    const event = eventFactory.createBuilder('event');
-    const eventProducer = eventFactory.createBuilder('producer');
-    const eventReceiver = eventFactory.createBuilder('receiver');
-
-    // Append Items to Basic
-    basicBuilder
-        .addString('Name', 'Krash')
-        .addString('Description', 'The best description ever.')
-        .addInteger('ID', 1);
-
-    console.log({
-        basicBuilder,
-        advancedBuilder,
-        defaultBuilder
-    });
-
-
-    // Build Out each builder
-    event
-        .addID(1)
-        .addName('Event1');
-
-    eventProducer
-        .addID(1)
-        .addName('EventProducer1');
-
-    eventReceiver
-        .addID(1)
-        .addName('EventReceiver1')
-
-    console.log({
-        event,
-        eventProducer,
-        eventReceiver
-    });
+    // Instantiate Systems
+    const paramsFacade = new ParamsFacade();
+    const eventFacade = new EventFacade();
+    // Log Data
+    paramsFacade.testParams();
+    eventFacade.testEvents();
 }
